7fbbe4d452fa7db02b938d9f3806231e1da2058a
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-02-24 08:29:08 -0700

    fmt

--- rnpix/pkcli/indexer.py
+++ rnpix/pkcli/indexer.py
@@ -3,6 +3,7 @@
 :copyright: Copyright (c) 2016 Robert Nagler.  All Rights Reserved.
 :license: http://www.apache.org/licenses/LICENSE-2.0.html
 """
+
 from pykern import pkio
 from pykern.pkdebug import pkdp
 import json
